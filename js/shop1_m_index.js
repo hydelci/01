3aa658fa85ef0cafa48d03f778fba8b6150438f1
hydelci <44991371+hydelci@users.noreply.github.com>
2018-11-28 16:00:29 +0900

    shop js파일 수정
    shop js파일 수정

--- js/shop1_m_index.js
+++ js/shop1_m_index.js
@@ -8,17 +8,6 @@ $('header .gnb').mouseleave(function(){
 });
 */
 
-	var userAgent = navigator.userAgent;
-	//alert(userAgent);
-
- //스마트폰 구분	
- var smartPhones = ['iphone', 'ipad', 'android'];
- for(var i in smartPhones) {
- 	if(navigator.userAgent.toLowerCase().match(new RegExp(smartPhones[i]))) {
- 		alert('스마트폰 페이지로 이동합니다.');
- 		location = 'https://hydelci.github.io/01/html/shop1_m_index.html';//이동하고자 하는 주소 작성 (기터브 내주소)
- 	}
- }
 
 //웹 nav 항목별 슬라이드
 $('.title').mouseenter(function(){
