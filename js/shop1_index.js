c24a3659d76a0694b4c01e49bcf383a5c2113f4b
hydelci <44991371+hydelci@users.noreply.github.com>
2018-11-28 16:12:03 +0900

    shop js파일 수정
    shop js파일 수정

--- js/shop1_index.js
+++ js/shop1_index.js
@@ -8,6 +8,18 @@ $('header .gnb').mouseleave(function(){
 });
 */
 
+	var userAgent = navigator.userAgent;
+	//alert(userAgent);
+
+ //스마트폰 구분	
+ var smartPhones = ['iphone', 'ipad', 'android'];
+ for(var i in smartPhones) {
+ 	if(navigator.userAgent.toLowerCase().match(new RegExp(smartPhones[i]))) {
+ 		alert('스마트폰 페이지로 이동합니다.').stop();
+ 		location = 'https://hydelci.github.io/01/html/shop1_m_index.html';//이동하고자 하는 주소 작성 (기터브 내주소)
+ 	}
+ }
+
 //웹 nav 항목별 슬라이드
 $('.title').mouseenter(function(){
 	$(this).next().stop().slideDown();
@@ -89,14 +101,3 @@ $('.m_icon1').click(function(){
 });
 
 
-	var userAgent = navigator.userAgent;
-	//alert(userAgent);
-
- //스마트폰 구분	
- var smartPhones = ['iphone', 'ipad', 'android'];
- for(var i in smartPhones) {
- 	if(navigator.userAgent.toLowerCase().match(new RegExp(smartPhones[i]))) {
- 		alert('스마트폰 페이지로 이동합니다.');
- 		location = 'https://hydelci.github.io/01/html/shop1_m_index.html';//이동하고자 하는 주소 작성 (기터브 내주소)
- 	}
- }
